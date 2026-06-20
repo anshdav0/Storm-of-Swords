eca122e3b35c40814af8f6cfee5244e4d5a4e468
ansh <anshdav0@gmail.com>
2026-06-20 17:48:28 +0530

    feat: added central loop that starts battle runs the logic and gives result

--- backend/internal/game/battle.go
+++ backend/internal/game/battle.go
@@ -37,3 +37,95 @@ func (t *SimTroop) findNearestBuilding(state *BattleState) *SimBuilding {
 
 	return nearest
 }
+
+func StartBattle(input BattleInput) ([]BattleEvent, BattleResult) {
+	state := makeState(input)
+	var allEvents []BattleEvent
+
+	const battleTimeLimit = 600
+
+	for tick := 0; tick < battleTimeLimit; tick++ {
+		state.CurrentTime = float64(tick) + 1.0
+
+		for i := range state.Buildings {
+			events := state.Buildings[i].Act(state)
+			allEvents = append(allEvents, events...)
+		}
+
+		for i := range state.Troops {
+			events := state.Troops[i].Act(state)
+			allEvents = append(allEvents, events...)
+		}
+
+		if allTroopsDead(state) || allBuildingDestroyed(state) {
+			break
+		}
+	}
+
+	result := computeResult(state)
+	return allEvents, result
+}
+
+func makeState(input BattleInput) *BattleState {
+	state := &BattleState{}
+
+	for _, sb := range input.DefenderSnapshot {
+		state.Buildings = append(state.Buildings, SimBuilding{
+			VillageBuildingID: sb.VillageBuildingID,
+			BuildingType:      sb.BuildingType,
+			CenterX:           float64(sb.XCor) + float64(sb.SizeX)/2.0,
+			CenterY:           float64(sb.YCor) + float64(sb.SizeY)/2.0,
+			CurrentHP:         sb.CurrentHP,
+			DPS:               sb.DPS,
+			Range:             sb.Range,
+			SplashRad:         sb.SplashRad,
+			Destroyed:         false,
+		})
+	}
+
+	instanceID := 0
+	for _, dt := range input.AttackerDeployment {
+		for i := 0; i < dt.Quantity; i++ {
+			state.Troops = append(state.Troops, SimTroop{
+				InstanceID: instanceID,
+				TroopID:    dt.TroopID,
+				TroopType:  dt.TroopType,
+				X:          float64(dt.X),
+				Y:          float64(dt.Y),
+				CurrentHP:  dt.HP,
+				Damage:     dt.Damage,
+				Speed:      dt.Speed,
+				Dead:       false,
+			})
+			instanceID++
+		}
+	}
+
+	return state
+}
+
+func computeResult(state *BattleState) BattleResult {
+	total := len(state.Buildings)
+	destroyed := 0
+	//mainCastleDestroyed := false
+
+	for _, b := range state.Buildings {
+		if b.Destroyed {
+			destroyed++
+		}
+	}
+
+	stars := 0
+	if total > 0 && float64(destroyed)/float64(total) >= 0.5 {
+		stars++
+	}
+	if destroyed == total && total > 0 {
+		stars++
+	}
+
+	return BattleResult{
+		StarsEarned:        stars,
+		TotalBuildings:     total,
+		BuildingsDestroyed: destroyed,
+	}
+}
